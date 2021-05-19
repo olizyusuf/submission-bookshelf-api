192445e81e294d4d9eb416231f79b29c244b4f4c
olizyusuf <olizyusuf@gmail.com>
2021-05-19 15:37:39 +0700

    sampai respone 201 sukses ditambahkan

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,44 @@
-const addBookHandler = (request, h) => {
+const { nanoid } = require('nanoid');
+const books = require('./bookshelf');
 
+const addBookHandler = (request, h) => {
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+
+  const id = nanoid(16);
+  const finished = (pageCount === readPage);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+
+  const newBook = {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+    id,
+    finished,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
 };
 
 module.exports = { addBookHandler };
